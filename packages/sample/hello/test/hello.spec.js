65d54061192729bf461e9c0bbc62e670fc1642ec
Emre Barack Sokullu <esokullu@gmail.com>
2022-06-24 12:22:16 -0400

    Update hello.spec.js

--- packages/sample/hello/test/hello.spec.js
+++ packages/sample/hello/test/hello.spec.js
@@ -3,6 +3,6 @@ const { main } = require("../hello");
 describe("Test App", () => {
   test("Hello", () => {
     let response = main({ name: "test_name" });
-    expect(response.body).toEqual("Hello test_name!");
+    expect(response.body).toEqual("Merhaba test_name!");
   });
 });
